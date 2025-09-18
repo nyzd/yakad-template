bf636bd91981146b44364a70208a2b748157aa88
al-abd <nexnema@gmail.com>
2025-09-18 17:39:33 +0330

    test dropdawn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { H1, Main, Screen } from "@yakad/ui";
+import LangSelector from "@/components/LangSelector";
 
 export default function Page() {
     return (
@@ -7,6 +8,7 @@ export default function Page() {
             <Main align="center">
                 <H1>Hi From Page</H1>
                 <Link href="/settings">Go to Settings</Link>
+                <LangSelector />
             </Main>
         </Screen>
     );
