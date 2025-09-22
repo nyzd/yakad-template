e18107c5cbed806686fa77c53ad1b456661b5450
al-abd <nexnema@gmail.com>
2025-09-22 14:59:30 +0330

    new yakad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 import Link from "next/link";
-import { Button, H1, Main, Row, Screen, Spacer, WithDropdawn } from "@yakad/ui";
+import {
+    Button,
+    Card,
+    Container,
+    H1,
+    H2,
+    H3,
+    Main,
+    Row,
+    Screen,
+    Spacer,
+    WithDropdawn,
+} from "@yakad/ui";
+import classNames from "classnames";
 
 const colorVars = [
     "--primaryColor",
@@ -35,73 +48,82 @@ const colorVars = [
 export default function Page() {
     return (
         <Screen>
-            <Main align="space">
-                <Row>
-                    zzz
-                    <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                        <Button variant="filled">zzYzz</Button>
-                    </WithDropdawn>
-                    <Spacer />
-                    <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                        <Button variant="filled">zzYzz</Button>
-                    </WithDropdawn>
-                    zzz
-                </Row>
-                <Row>
-                    ZZZZZZZZZZZZZZZZZ
-                    <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                        <Button variant="filled">zzYzz</Button>
-                    </WithDropdawn>
-                    <Spacer />
-                    <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                        <Button variant="filled">zzYzz</Button>
-                    </WithDropdawn>
-                    ZZZZZZZZZZZZZZZZZ
-                </Row>
-                <H1>Hi From Page</H1>
-                <Link href="/settings">Go to Settings</Link>
-                <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                    <Button variant="filled">Select</Button>
-                </WithDropdawn>
-                <div
-                    style={{
-                        padding: "1rem",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        gap: "12px",
-                    }}
-                >
-                    {colorVars.map((varName) => (
-                        <div
-                            key={varName}
-                            style={{
-                                backgroundColor: `rgb(var(${varName}))`,
-                                color: "white",
-                                padding: "1rem",
-                                borderRadius: "8px",
-                                width: "300px",
-                                boxShadow: "0 0 6px rgba(0,0,0,0.3)",
-                                fontFamily: "monospace",
-                                textAlign: "center",
-                                userSelect: "none",
-                            }}
-                        >
-                            {varName}
-                        </div>
-                    ))}
-                </div>
-                <Row>
-                    zzz
+            <Main>
+                <Container size="md" align="space">
+                    <Row>
+                        zzz
+                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        <Spacer />
+                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        zzz
+                    </Row>
+                    <Row>
+                        ZZZZZZZZZZZZZZZZZ
+                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        <Spacer />
+                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        ZZZZZZZZZZZZZZZZZ
+                    </Row>
+                    <H1>Hi From Page</H1>
                     <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                        <Button variant="filled">zzYzz</Button>
+                        <Button variant="filled">Select</Button>
                     </WithDropdawn>
-                    <Spacer />
-                    <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                        <Button variant="filled">zzYzz</Button>
-                    </WithDropdawn>
-                    zzz
-                </Row>
+                    <Card
+                        style={{
+                            gap: "12px",
+                        }}
+                    >
+                        <Card>
+                            <Card>
+                                <Link href="/settings">
+                                    <Button variant="outlined">
+                                        Go to Settings
+                                    </Button>
+                                </Link>
+                                <H3>Level 3 Card</H3>
+                            </Card>
+                            <H2>Level 2 Card</H2>
+                        </Card>
+                        <H1>Level 1 Card</H1>
+                        {colorVars.map((varName) => (
+                            <div
+                                key={varName}
+                                style={{
+                                    backgroundColor: `rgb(var(${varName}))`,
+                                    color: "white",
+                                    padding: "1rem",
+                                    borderRadius: "8px",
+                                    width: "300px",
+                                    boxShadow: "0 0 6px rgba(0,0,0,0.3)",
+                                    fontFamily: "monospace",
+                                    textAlign: "center",
+                                    userSelect: "none",
+                                }}
+                            >
+                                {varName}
+                            </div>
+                        ))}
+                    </Card>
+                    <Row>
+                        zzz
+                        <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        <Spacer />
+                        <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
+                            <Button variant="filled">zzYzz</Button>
+                        </WithDropdawn>
+                        zzz
+                    </Row>
+                </Container>
             </Main>
         </Screen>
     );
