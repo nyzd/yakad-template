81033ec380e029a30dc57c6784033c4393ef9972
al-abd <nexnema@gmail.com>
2025-10-04 23:14:15 +0330

    clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ import {
     Row,
     Screen,
     Spacer,
+    Text,
     WithDropdawn,
 } from "@yakad/ui";
-import classNames from "classnames";
 
 const colorVars = [
     "--primaryColor",
@@ -48,94 +48,12 @@ const colorVars = [
 export default function Page() {
     return (
         <Screen>
-            <Main>
-                <Container size="md" align="space">
-                    <Row>
-                        zzz
-                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        <Spacer />
-                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        zzz
-                    </Row>
-                    <Row>
-                        ZZZZZZZZZZZZZZZZZ
-                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        <Spacer />
-                        <WithDropdawn dropdawnChildren={<Dropdawn />}>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        ZZZZZZZZZZZZZZZZZ
-                    </Row>
-                    <H1>Hi From Page</H1>
-                    <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                        <Button variant="filled">Select</Button>
-                    </WithDropdawn>
-                    <Card
-                        style={{
-                            gap: "12px",
-                        }}
-                    >
-                        <Card>
-                            <Card>
-                                <Link href="/settings">
-                                    <Button variant="outlined">
-                                        Go to Settings
-                                    </Button>
-                                </Link>
-                                <H3>Level 3 Card</H3>
-                            </Card>
-                            <H2>Level 2 Card</H2>
-                        </Card>
-                        <H1>Level 1 Card</H1>
-                        {colorVars.map((varName) => (
-                            <div
-                                key={varName}
-                                style={{
-                                    backgroundColor: `rgb(var(${varName}))`,
-                                    color: "white",
-                                    padding: "1rem",
-                                    borderRadius: "8px",
-                                    width: "300px",
-                                    boxShadow: "0 0 6px rgba(0,0,0,0.3)",
-                                    fontFamily: "monospace",
-                                    textAlign: "center",
-                                    userSelect: "none",
-                                }}
-                            >
-                                {varName}
-                            </div>
-                        ))}
-                    </Card>
-                    <Row>
-                        zzz
-                        <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        <Spacer />
-                        <WithDropdawn dropdawnChildren={<Dropdawn />} blur>
-                            <Button variant="filled">zzYzz</Button>
-                        </WithDropdawn>
-                        zzz
-                    </Row>
-                </Container>
+            <Main align="space">
+                <Text variant="heading2">Hi from Yakad Template!</Text>
+                <Link href="/settings">
+                    <Button variant="filled">Settings</Button>
+                </Link>
             </Main>
         </Screen>
     );
 }
-
-const Dropdawn = () => (
-    <div style={{ width: "200px" }}>
-        <Button>TTTT</Button>
-        <Button>Test</Button>
-        <Button>Test</Button>
-        <Button>Test</Button>
-        <Button>Test</Button>
-        <Button>Test</Button>
-    </div>
-);
